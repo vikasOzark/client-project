03cbd4d29303b239311d7984e86e17ae8f19b7fc
Vikas Kumar <80447331+vikasOzark@users.noreply.github.com>
2023-07-15 21:01:20 +0530

    all changes with new file structure

---
+++ static/main/js/dashboard.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const adminToggleFun = () => {
+    const adminToggle = document.querySelector('#admin_control');
+    adminToggle.addEventListener('click', ()=>{
+        
+    })
+}
+
