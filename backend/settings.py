b8e80d430524c1a1f2d35e045777b9a91a1d4070
Vikas Kumar <80447331+vikasOzark@users.noreply.github.com>
2023-08-03 22:21:21 +0530

    added changes

--- backend/settings.py
+++ backend/settings.py
@@ -85,6 +85,18 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default" : {
+#         "ENGINE" : "django.db.backends.postgresql",
+#         "NAME" : "blackrokinvest_db",
+#         "USER" : "blackrokinvest",
+#         "PASSWORD" : "Blackrokinvest@1234##",
+#         "HOST" : "127.0.0.1",              
+#         "PORT" : "5432",
+#         }
+# }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
