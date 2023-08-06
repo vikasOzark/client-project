d950195e904062cf828172c74004e411714704f3
Vikas Kumar <80447331+vikasOzark@users.noreply.github.com>
2023-08-06 22:30:33 +0530

    compression removed

--- backend/settings.py
+++ backend/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  "root/blackrokinvest/static"
+STATIC_ROOT =  os.path.join(BASE_DIR,"static")
 STATICFILES_DIRS =  [os.path.join(BASE_DIR,"staticfiles")]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- static/main/js/marquee.js
+++ static/main/js/marquee.js
@@ -1,35 +1,36 @@
 "use strict";
 
-const MarqueeTopToBottom = (selector, speed) => {
-  const parentSelector = document.querySelector(selector);
-  const clone = parentSelector.innerHTML;
-  const firstElement = parentSelector.children[0];
-  const firstElementClientWidth = firstElement.clientWidth
-  let i = 0;
-  let marqueeInterval;
-  
-  parentSelector.insertAdjacentHTML('beforeend', clone);
-  parentSelector.insertAdjacentHTML('beforeend', clone);
-  const startMarquee = () => {
-    marqueeInterval = setInterval(() =>{
-      firstElement.style.marginTop = `-${i}px`;
-      if(i > firstElementClientWidth) {
-        i = 0;
-      }
-      i = i + speed;
-    },10)
+const Marquee = (selector, speed) => {
+    const parentSelector = document.querySelector(selector);
+    const clone = parentSelector.innerHTML;
+    const firstElement = parentSelector.children[0];
+    const firstElementClientWidth = firstElement.clientWidth
+    let i = 0;
+    let marqueeInterval;
+    
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+    const startMarquee = () => {
+      marqueeInterval = setInterval(() =>{
+        firstElement.style.marginLeft = `-${i}px`;
+        if(i > firstElementClientWidth) {
+          i = 0;
+        }
+        i = i + speed;
+      },10)
+    }
+    
+    
+    const stopMarquee = () => {
+      clearInterval(marqueeInterval);
+    }
+    
+    parentSelector.addEventListener('mouseenter', stopMarquee);
+    parentSelector.addEventListener('mouseleave', startMarquee);
+    
+    startMarquee();
   }
   
-  const stopMarquee = () => {
-    clearInterval(marqueeInterval);
-  }
-  
-  parentSelector.addEventListener('mouseenter', stopMarquee);
-  parentSelector.addEventListener('mouseleave', startMarquee);
-  
-  startMarquee();
-}
-
-window.addEventListener('load', () => {
-  MarqueeTopToBottom('.marquee-top-bottom', .4)
-});
+  window.addEventListener('load', () => {
+    Marquee('.marquee', .4)
+  });
