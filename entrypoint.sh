798414fb2325dbc111d34577ee0faafb75a589f4
shubhanesh <shubhanesh@netwebindia.com>
2023-08-09 21:58:05 +0530

    abc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 echo "running"
 python manage.py makemigrations --no-input
+echo "migration complete"
 python manage.py migrate --no-input
+echo "migrate complete"
 python manage.py collectstatic --no-input
+echo "collect static complete"
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --reload --timeout 900
+echo "bind gunicorn"
